8ea4fc37367dc44ad1bf3760c76b5a2cf898ab38
Randy Green <randywgreen@gmail.com>
2026-01-26 09:04:44 -0600

    Initial Commit

---
+++ Evolving Ambience/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Evolving Ambience
+//
+//  Created by RANDY GREEN on 1/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Evolving Ambience/Evolving_AmbienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Evolving_AmbienceApp.swift
+//  Evolving Ambience
+//
+//  Created by RANDY GREEN on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Evolving_AmbienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
